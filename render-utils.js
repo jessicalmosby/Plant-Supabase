3d4f51a155b94e91f080f6941bc397bd7c88f6ed
Jessica Mosby <jessicamosby@mac.com>
2022-11-15 12:19:51 -0500

    added renderPlantDetail in render-utils

--- render-utils.js
+++ render-utils.js
@@ -16,3 +16,37 @@ export function renderPlantCard(plant) {
 
     return a;
 }
+
+export function renderPlantDetail(plant) {
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    const descriptionEl = document.createElement('p');
+    const nameEl = document.createElement('p');
+    const sunEl = document.createElement('p');
+    const petEl = document.createElement('p');
+    const sunAndPetEl = document.createElement('div');
+
+    div.classList.add('plant-detail');
+
+    nameEl.textContent = plant.name;
+    nameEl.classList.add('name');
+
+    descriptionEl.textContent = plant.description;
+    descriptionEl.classList.add('description');
+
+    sunEl.textContent = `${plant.sun}`;
+    sunEl.classList.add('sun');
+
+    petEl.textContent = `${plant.pet}`;
+    petEl.classList.add('pet');
+
+    sunAndPetEl.classList.add('sun-and-pet');
+
+    sunAndPetEl.append(sunEl, petEl);
+
+    img.src = `../assets/${plant.type}.jpeg`;
+
+    div.append(nameEl, img, sunAndPetEl, descriptionEl);
+
+    return div;
+}
