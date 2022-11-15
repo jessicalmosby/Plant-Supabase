bde4e99fbc2013f8d5198a9e380a04976885b66b
Jessica Mosby <jessicamosby@mac.com>
2022-11-15 13:32:45 -0500

    fixed syntx error in fetch-utils & changed supabase titles to match my code

--- fetch-utils.js
+++ fetch-utils.js
@@ -2,14 +2,15 @@ const SUPABASE_URL = 'https://uuxnounuxbgphldtwpld.supabase.co';
 const SUPABASE_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1eG5vdW51eGJncGhsZHR3cGxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgwNjksImV4cCI6MTk4MzY4NDA2OX0.X0has-XP8I6mpbJtyM-bfmgJBZ7Iy0neL2xNIHEyxJc';
 
-const client = supabase.createclient(SUPABASE_URL, SUPABASE_KEY);
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function getPlants() {
-    const response = await client.from('plants').select('*');
+    const response = await client.from('Plants').select('*');
+    console.log(response);
     return response.data;
 }
 
 export async function getPlant(id) {
-    const response = await client.from('plants').select('*').match({ id: id }).single();
+    const response = await client.from('Plants').select('*').match({ id: id }).single();
     return response.data;
 }
